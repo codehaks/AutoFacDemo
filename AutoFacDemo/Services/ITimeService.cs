e87231401e0e85cb8b0bd7ca80d66232cb463855
Hakim Ghods <ghods.hakim@hotmail.com>
2020-03-09 04:55:51 +0330

    Add project files.

---
+++ AutoFacDemo/Services/ITimeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoFacDemo.Services
+{
+    public interface ITimeService
+    {
+        string GetNow();
+    }
+
+    public class TimeService : ITimeService
+    {
+        public string GetNow()
+        {
+            return DateTime.Now.ToShortTimeString();
+        }
+    }
+}
---
+++ AutoFacDemo/Startup.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutoFacDemo.Controllers;
+using AutoFacDemo.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AutoFacDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews()
+                .AddControllersAsServices(); 
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+
+            builder.RegisterType<HomeController>().PropertiesAutowired();
+            builder.RegisterType<TimeService>().PropertiesAutowired().InstancePerLifetimeScope();
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
